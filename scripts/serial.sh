e776d3ee2d52973a49672976a5458ed902488d59
jkrue242 <josephkrueger242@gmail.com>
2025-03-13 18:28:40 -0500

    Movement detection updates

--- scripts/serial.sh
+++ scripts/serial.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 # update this to the port of your ESP8266
-USB_PORT=/dev/cu.usbserial-130
+USB_PORT=/dev/cu.usbserial-140
 
 # update this to the baud rate of your ESP8266
 BAUD_RATE=9600
